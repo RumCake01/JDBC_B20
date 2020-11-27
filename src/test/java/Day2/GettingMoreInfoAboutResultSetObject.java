cc7d84f10f22ffa38b6856bdc1edbe39cff9a81f
RumCake <mia.hasnutdinova@gmail.com>
2020-11-26 21:41:08 -0500

    Getting More Info about ResultSet Object

--- src/test/java/Day2/GettingMoreInfoAboutResultSetObject.java
+++ src/test/java/Day2/GettingMoreInfoAboutResultSetObject.java
@@ -13,7 +13,34 @@ public class GettingMoreInfoAboutResultSetObject {
 
         Statement statement = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
 
-        ResultSet rs = statement.executeQuery("SELECT * FROM JOBS");
+        ResultSet rs = statement.executeQuery("SELECT * FROM EMPLOYEES");
+
+        // to get more information about ResultSEt, we can get entire table which is the MetaData
+        // MetaData is data about the data, not what is inside the columns or rows
+        // resultSetMetaData - is data about the ResultSet object than contain our resulting rows and columns, it has the readl data.
+        // for ex: column names, column counts etc
+
+        // this is how we get the ResultSet
+
+        ResultSetMetaData rsmd = rs.getMetaData();
+        // getmetaData - doesnt contain the actual result we got by our query, it does not provide any values of the resultSet
+        // it has information about the result table that we got from our query
+        // ResultSetMetaData - itself does not contain any row data, but only information about the columns
+
+        // we only need 2 methods from this to get column count and column name / label
+        int colCount = rsmd.getColumnCount();
+        System.out.println("column count"+colCount);
+
+        // to get the column name we can use - getColumnLabel method:
+
+        System.out.println("First columns Name is " + rsmd.getColumnLabel(1));
+        System.out.println("Second column name is " + rsmd.getColumnLabel(2));
+
+        // get all the column names:
+        for(int colNum = 1; colNum <=colCount; colNum++){
+            System.out.println("Column name "+ rsmd.getColumnLabel(colNum));
+
+        }
 
     }
 }
