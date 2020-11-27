13cba6afcb704cf0659389395809db158bc0f45d
RumCake <mia.hasnutdinova@gmail.com>
2020-11-26 21:45:28 -0500

    DisplayingAllDataFromAllColumn

--- src/test/java/Day2/DisplayingAllDataFromAllColumn.java
+++ src/test/java/Day2/DisplayingAllDataFromAllColumn.java
@@ -1,4 +1,24 @@
 package Day2;
 
+import java.sql.*;
+
 public class DisplayingAllDataFromAllColumn {
+
+    public static void main(String[] args) throws SQLException {
+
+        String connection = "jdbc:oracle:thin:@54.172.140.77:1521:XE";
+        String username = "hr";
+        String password = "hr";
+
+        Connection conn = DriverManager.getConnection(connection, username, password);
+
+        Statement statement = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+
+        ResultSet rs = statement.executeQuery("SELECT * FROM EMPLOYEES");
+
+
+
+
+
+    }
 }
