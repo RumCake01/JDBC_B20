89de4016f01243f12385998a36632a2416c5fbed
RumCake <mia.hasnutdinova@gmail.com>
2020-11-28 23:31:08 -0500

    added junit dependency and used assertions

---
+++ src/test/java/Day4/DB_Test.java
@@ -0,0 +1,14 @@
+package Day4;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DB_Test {
+
+    @Test
+    public void test1(){
+
+        Assertions.assertEquals(9,3*3);
+
+    }
+}
